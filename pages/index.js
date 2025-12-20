1f1ca557972ce353d960a7d060b467bfeceb5d90
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-12-20 22:25:39 +0330

    create context on home page for  forward tours data on app

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
 import Header from '@/components/header'
 import Main from '@/components/template/main'
-import React from 'react'
+import React, { createContext } from 'react'
 
-function Index({tours}) {
-  console.log(tours)
+export const TourContext = createContext()
+function Index({ tours }) {
   return (
     <>
-    <Main/>
+      <TourContext.Provider value={tours}>
+        <Main />
+      </TourContext.Provider>
     </>
   )
 }
 
 export default Index
 
-export async function getServerSideProps(){
-     const api=process.env.NEXT_PUBLIC_API_URL
-    const res = await fetch(`${api}/tour`)
-    const tours= await res.json()
-    return{
-        props:{tours}
-    }
+export async function getServerSideProps() {
+  const api = process.env.NEXT_PUBLIC_API_URL
+  const res = await fetch(`${api}/tour`)
+  const tours = await res.json()
+  return {
+    props: { tours }
+  }
 }
