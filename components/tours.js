26fbb0a18e286e29b3a2a0c18b17f30f7cdff9ad
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-12-21 12:18:43 +0330

    complete tours component

--- components/tours.js
+++ components/tours.js
@@ -1,26 +1,44 @@
 import axios from 'axios'
 import React, { useEffect, useState, useContext } from 'react'
+import Image from 'next/image'
 import { TourContext } from '@/pages'
+import Link from 'next/link'
 
 function Tours() {
     const { tours } = useContext(TourContext)
+
+    const formatFaNumber = (value) => Number(value).toLocaleString('en-US').replace(/\d/g, d => '۰۱۲۳۴۵۶۷۸۹'[d])
+
     return (
         <>
-            <div className='w-full flex justify-center'>
-                <div className=' w-[80vw] font-normal text-3xl text-[#000000]'>
-                    <h1>همه تور ها</h1>
-                    {tours.map((tour)=>{
-                        console.log(tour)
-                    })
+            <div className='w-full flex flex-col justify-center items-center'>
+                <h1 className='w-[80vw] font-normal text-[32px]'>همه تور ها</h1>
+                <div className="w-[80vw] mx-auto bg-amber-6 00 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+                    {/* <div className="w-[80vw] bg-amber-600 grid grid-cols-4 gap-5 mx-auto  font-normal text-3xl text-black"> */}
+                    {/* <div className='w-[80vw] bg-amber-600 flex flex-wrap justify-between gap-5 font-normal  text-3xl text-[#000000]'> */}
+                    {tours.map((tour) => (
+                        <div key={tour.id} className='box-content size-68.5 rounded-xl border border-[#D9D9D9]'>
+                            <img src={tour.image} alt='tour image' />
+                            <h2 className=' font-medium text-2xl  p-1'>{tour.title}</h2>
+                            <div className=' h-7 overflow-hidden font-normal text-[15px] text-[#282828B2] p-2 '>
+                                {tour.options.map((option) => (
+                                    <>{option}| </>
+                                ))}</div>
+
+                            <div className='flex justify-between items-center p-2  h-10 w-full border-t border-[#D9D9D9]'>
+                                <button className=' w-16.5 h-6.5 bg-[#28A745] rounded-sm font-normal text-[15px] text-[#FFFFFF] cursor-pointer' ><Link href={`/${tour.id}`}>رزرو</Link></button>
+                                <div className='flex items-center gap-2 p-2'>
+                                    <span className='font-normal text-[16px] text-[#009ECA]'>{formatFaNumber(tour.price)}</span>
+                                    <span className='font-normal text-[12px]'> تومان</span>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    ))
                     }
-
-                    <div className=' bg-amber-400 size-68.5 rounded-xl'>
-
-                    </div>
-
-
                 </div>
-
 
             </div>
         </>
