059600f478ab0d927056709a8ce376abbdb6f8ec
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-12-21 13:43:46 +0330

    use ssr on dynamic route tour details and fetch tour data

--- pages/[tourId].js
+++ pages/[tourId].js
@@ -1,9 +1,23 @@
 import React from 'react'
-
-function TourId() {
+import { useRouter } from 'next/router'
+function TourId({tour}) {
+    const router=useRouter()
+    const {tourId}=router.query
+    console.log(tourId)
+    console.log(tour)
   return (
     <div>TourId</div>
   )
 }
 
 export default TourId
+
+export async function getServerSideProps(context) {
+  const id=context.query.tourId
+  const api = process.env.NEXT_PUBLIC_API_URL
+  const res = await fetch(`${api}/tour/${id}`)
+  const tour = await res.json()
+  return {
+    props: { tour }
+  }
+}
