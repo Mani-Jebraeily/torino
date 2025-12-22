2939ec2184e03c72bf05c674c0eef12b19069d14
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-12-22 19:16:19 +0330

    create github component and wrap on app in _document page

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Footer from "@/components/footer";
+import GitHub from "@/components/gitHub";
 import Header from "@/components/header";
 import { Html, Head, Main, NextScript } from "next/document";
 
@@ -9,6 +10,7 @@ export default function Document() {
       <body className="antialiased">
         <Header/>
         <Main />
+        <GitHub/>
         <Footer/>
         <NextScript />
       </body>
