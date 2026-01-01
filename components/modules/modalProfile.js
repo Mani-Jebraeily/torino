6accdc095952b68c7483d2920314f1641aae727a
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2026-01-01 19:51:14 +0330

    update  modal profile

--- components/modules/modalProfile.js
+++ components/modules/modalProfile.js
@@ -3,6 +3,7 @@ import profile from '@/public/profile.svg'
 import logout from '@/public/logout.svg'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 
 
@@ -18,10 +19,10 @@ function ModalProfile() {
     }
     return (
         <div className=' absolute top-11 flex flex-col justify-center  gap-4 w-50  h-25 bg-[#FFFFFF] bg- red-100 rounded-[11px] '>
-            <div className='flex gap-2 cursor-pointer pr-1'>
+            <Link href="/profile" className='flex gap-2 cursor-pointer pr-1'>
                 <Image src={profile} alt='profile svg' />
                 <button className='cursor-pointer'>اطلاعات حساب کاربری</button>
-            </div>
+            </Link>
             <div className='flex gap-2 cursor-pointer pr-1'>
                 <Image src={logout} alt='logout svg' />
                 <button onClick={deleteTokenHandeler} className='font-normal text-[14px] text-[#D40000]'>خروج از حساب کاربری</button>
