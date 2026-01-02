d2ff8920e27888d9d96b76d66d29ddfde09e6a75
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2026-01-02 16:26:21 +0330

    create profilePart component

---
+++ components/profilePart.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Profile() {
+    return (
+
+        <div>
+            <div>
+                <div>
+                    <h1>اطلاعات حساب کاربری</h1>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile
--- pages/profile.js
+++ pages/profile.js
@@ -3,30 +3,35 @@ import Image from 'next/image'
 import userIcon from '@/public/userIcon.svg'
 import sun from '@/public/sun-fog.svg'
 import card from '@/public/convert-card.svg'
-import React, { use } from 'react'
+import React, { use, useState } from 'react'
+import ProfilePart from '@/components/profilePart'
 
 function Profile() {
+    const [showProfile,setProfile]=useState(true)
+
     return (
         <>
             <Header />
             <div>
-                <div className='w-71 h-42 flex flex-col border border-[#00000033] rounded-[10px]'>
-                    <button className='flex'>
+                <div className='w-71 h-42 flex flex-col border border-[#00000033] rounded-[10px] overflow-hidden'>
+                    <button className='flex items-center gap-2 cursor-pointer w-full h-[170px] '>
                         <Image src={userIcon} />
                         <p>پروفایل</p>
                     </button>
 
-                    <button className='flex'>
+                    <button className='flex items-center gap-2 cursor-pointer w-full h-[170px] border-t border-b border-[#00000033]' >
                         <Image src={sun} />
                         <p>تور های من</p>
                     </button>
 
-                    <button className='flex'>
+                    <button className='flex items-center gap-2 cursor-pointer w-full h-[170px]'>
                         <Image src={card} />
                         <p>تراکنش ها</p>
                     </button>
                 </div>
-                <div></div>
+                <div>
+                    {showProfile&&<ProfilePart/>}
+                </div>
             </div>
         </>
     )
