d7d1a77cb9fddf40d0b6705368943fba8318db0e
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2026-01-04 23:48:37 +0330

    create basket page and basket Template

---
+++ components/template/basketTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../header'
+
+function BasketTemplate() {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default BasketTemplate
---
+++ pages/basket.js
@@ -0,0 +1,10 @@
+import BasketTemplate from '@/components/template/basketTemplate'
+import React from 'react'
+
+function basket() {
+  return (
+    <BasketTemplate/>
+  )
+}
+
+export default basket
