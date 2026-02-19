af5f2dac503eb1071ab96cd830f1006a077e05d7
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2026-02-19 16:00:46 +0330

    responsive footer component

--- components/footer.js
+++ components/footer.js
@@ -13,8 +13,8 @@ function Footer() {
     return (
         <>
             <div className='flex items-center justify-center   w-full'>
-                <div className='pb-5 pt-5 w-[80vw] flex justify-between  border-t relative bottom-0 border-[#00000033]'>
-                    <div className='w-[20%] flex  gap-5 justify-between items-center ' >
+                <div className='pb-5 pt-5 w-[80vw] gap-5 flex flex-col sm:flex-row bg-amber800 justify-between  border-t relative bottom-0 border-[#00000033]'>
+                    <div className='w-full sm:w-[30%] flex  gap-5 sm:gap-0 justify-between items-center  ' >
                         <ul className=' min-w-fit flex flex-col gap-1 *:cursor-pointer  *:font-normal text-[18px]   text-[#282828]'>
                             <li className=' font-semibold cursor-default text-2xl text-[#282828] mb-5'>تورینو</li>
                             <li>درباره ما</li>
@@ -32,7 +32,7 @@ function Footer() {
                         </ul>
                     </div>
 
-                    <div className='w-[80%]'>
+                    <div className='w-full sm:w-[70%] flex justify-between flex-row-reverse sm:flex-col bg-blue -400'>
                         <div className=' h-[50%] felx items-center gap-10'>
                             <div className=' flex flex-col gap-5 items-end '>
                                 <Image src={logo} />
@@ -41,7 +41,7 @@ function Footer() {
 
                         </div>
 
-                        <div className=' *:min -w-10 *:w-[6vw] *:cursor-pointer flex gap-10 h-[50%] flex-row-reverse items-center start '>
+                        <div className=' *:min -w-10 *:w-[6vw] *:cursor-pointer flex w-40 sm:w-full bg-ambe r-500 flex-wrap gap-10 h-[50%] flex-row-reverse items-center start '>
                             <Image src={logo1} />
                             <Image src={logo2} />
                             <Image src={barcode1} />
